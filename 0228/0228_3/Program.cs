6a3f30b1a41662a3d4438d6ecb558dce200d0d97
Horea Oros <horea.oros@gmail.com>
2023-02-28 10:59:03 +0200

    add Rational project

---
+++ 0228/0228_3/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Rational r1 = new Rational(3, 4);
+        Console.WriteLine($"r1 = {r1}");
+        Rational r2 = new Rational(4, 6);
+        Console.WriteLine($"r2 = {r2}");
+        Rational r3 = r1.Add(r2);
+        Console.WriteLine($"r3 = {r3}");
+        Rational r4 = r1 + r2;
+        Console.WriteLine($"r4 = {r4}");
+        if (r3 == r4)
+            Console.WriteLine($"{r3} este egal cu {r4}");
+        else
+            Console.WriteLine($"{r3} nu este egal cu {r4}");
+    }
+}
---
+++ 0228/0228_3/Rational.cs
@@ -0,0 +1,48 @@
+﻿using System;
+internal class Rational
+{
+	int numarator, numitor;
+	public Rational()
+	{
+		numarator = numitor = 0;
+	}
+	public Rational(int numarator, int numitor)
+    {
+        this.numarator = numarator;
+        this.numitor = numitor;
+    }
+    public override string ToString()
+    {
+        return $"({this.numarator}/{this.numitor})";
+    }
+
+    internal Rational Add(Rational r2)
+    {
+        Rational result = new Rational(0, 0);
+        result.numitor = this.numitor * r2.numitor;
+        result.numarator = this.numarator * r2.numitor + 
+            r2.numarator * this.numitor;
+        result.Reduce();
+        return result;
+    }
+
+    public void Reduce()
+    {
+        int d = Gcd(Math.Abs(this.numarator),
+            Math.Abs(this.numitor));
+        this.numarator /= d;
+        this.numitor /= d;
+    }
+
+    private int Gcd(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        else
+            return Gcd(b, a % b);
+    }
+    public static Rational operator+(Rational r1, Rational r2)
+    {
+        return r1.Add(r2);
+    }
+}
