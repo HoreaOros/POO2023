17be39875708cd7dea21a4289fbbc94f27b0afd7
Horea Oros <horea.oros@gmail.com>
2023-03-14 09:01:39 +0200

    add march 14 project

---
+++ 0314/0314/MyDateTime.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+internal class MyDateTime
+{
+    private int zi;
+    private int luna;
+    private int an;
+    private int ora;
+    private int minut;
+    private int secunda;
+
+    public MyDateTime(int zi = 0, int luna = 0, int an = 0, int ora = 0, int minut = 0, int secunda = 0)
+    {
+        this.zi = zi;
+        this.luna = luna;
+        this.an = an;
+        this.ora = ora;
+        this.minut = minut;
+        this.secunda = secunda;
+    }
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append(this.zi)
+          .Append(".")
+          .Append(string.Format("{0,2:D2}", this.luna))
+          .Append(".")
+          .Append(this.an)
+          .Append(" ")
+          .Append(this.ora)
+          .Append(".")
+          .Append(this.minut)
+          .Append(".")
+          .Append(this.secunda);
+        return sb.ToString();
+    }
+
+}
---
+++ 0314/0314/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+DateTime dateTime = DateTime.Now;
+Console.WriteLine(dateTime);
+
+DateTime dateTimeUTC = DateTime.UtcNow;
+Console.WriteLine(dateTimeUTC);
+
+Console.WriteLine(dateTime.ToUniversalTime());
+Console.WriteLine(dateTime.ToShortDateString());
+Console.WriteLine(dateTime.ToShortTimeString());
+
+DateTime dateTime2 = dateTime.AddHours(3.5);
+Console.WriteLine(dateTime2);
+
+Console.WriteLine(dateTime.CompareTo(dateTime2));
+
+Console.WriteLine(dateTime == dateTime2);
+Console.WriteLine(dateTime < dateTime2);
+
+Console.WriteLine(dateTime - dateTime2);
+
+
+
+MyDateTime md = new MyDateTime(14, 3, 2023, 8, 59, 42);
+Console.WriteLine(md);
+
+MyDateTime md2 = new MyDateTime(23, 6, 2023);
+MyDateTime md3 = new MyDateTime(an: 2025, luna: 12, zi: 13);
+Console.WriteLine(md3);
+
+
