b00eca720700a6b1c331c3233d3be86de8f0d0d2
Horea Oros <horea.oros@gmail.com>
2023-03-21 08:54:58 +0200

    add Rectangle project

---
+++ 0321/Rectangle/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Rectangle
+{
+    public class Rectangle
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public Rectangle(int x, int y, int width, int height)
+        {
+            X = x; Y = y; Width = width; Height = height;
+        }
+        public Rectangle(Point p1, Point p2) 
+        { 
+        }
+    }
+}
